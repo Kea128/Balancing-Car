6daf74afa4063b072e29e09d38ddb3cb11c7296e
GYK <1287811578@qq.com>
2024-07-18 13:41:40 +0800

    ver_0718_第一次跑通

--- src/balancing_car_control/src/keyboard_cmd.cpp
+++ src/balancing_car_control/src/keyboard_cmd.cpp
@@ -1,5 +1,5 @@
-#include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
+#include <ros/ros.h>
 #include <stdio.h>
 #include <termios.h>
 
@@ -10,20 +10,18 @@ static float min_linear_vel = -20;
 static float max_angular_vel = 180;
 static float min_angular_vel = -180;
 
-int GetCh()
-{
+int GetCh() {
   static struct termios oldt, newt;
-  tcgetattr( STDIN_FILENO, &oldt);
+  tcgetattr(STDIN_FILENO, &oldt);
   newt = oldt;
   newt.c_lflag &= ~(ICANON);
-  tcsetattr( STDIN_FILENO, TCSANOW, &newt);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
   int c = getchar();
-  tcsetattr( STDIN_FILENO, TCSANOW, &oldt);
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
   return c;
 }
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
   ros::init(argc, argv, "keyboard_cmd");
 
   printf("Keyboard controlled balance car: \n");
@@ -36,68 +34,64 @@ int main(int argc, char** argv)
   printf("------------- \n");
 
   ros::NodeHandle n;
-  ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("/my_cmd_vel", 10);
+  ros::Publisher cmd_vel_pub =
+      n.advertise<geometry_msgs::Twist>("/my_cmd_vel", 10);
 
   geometry_msgs::Twist base_cmd;
   base_cmd.linear.x = 0;
   base_cmd.linear.y = 0;
   base_cmd.angular.z = 0;
 
-  while(n.ok())
-  {
+  while (n.ok()) {
     int cKey = GetCh();
-    if(cKey=='w')
-    {
+    if (cKey == 'w') {
       base_cmd.linear.x += linear_vel;
-      if(base_cmd.linear.x > max_linear_vel) base_cmd.linear.x = max_linear_vel;
+      if (base_cmd.linear.x > max_linear_vel)
+        base_cmd.linear.x = max_linear_vel;
       cmd_vel_pub.publish(base_cmd);
-      printf(" - linear.x= %.2f linear.y= %.2f angular.z= %.2f \n",base_cmd.linear.x,base_cmd.linear.y,base_cmd.angular.z);
-    } 
-    else if(cKey=='s')
-    {
+      printf(" - linear.x= %.2f linear.y= %.2f angular.z= %.2f \n",
+             base_cmd.linear.x, base_cmd.linear.y, base_cmd.angular.z);
+    } else if (cKey == 's') {
       base_cmd.linear.x += -linear_vel;
 
-      if(base_cmd.linear.x < min_linear_vel) base_cmd.linear.x = min_linear_vel;
+      if (base_cmd.linear.x < min_linear_vel)
+        base_cmd.linear.x = min_linear_vel;
       cmd_vel_pub.publish(base_cmd);
-      printf(" - linear.x= %.2f linear.y= %.2f angular.z= %.2f \n",base_cmd.linear.x,base_cmd.linear.y,base_cmd.angular.z);
-    } 
-    else if(cKey=='a')
-    {
+      printf(" - linear.x= %.2f linear.y= %.2f angular.z= %.2f \n",
+             base_cmd.linear.x, base_cmd.linear.y, base_cmd.angular.z);
+    } else if (cKey == 'a') {
       base_cmd.angular.z += angular_vel;
-      if(base_cmd.angular.z > max_angular_vel) base_cmd.angular.z = max_angular_vel;
+      if (base_cmd.angular.z > max_angular_vel)
+        base_cmd.angular.z = max_angular_vel;
       cmd_vel_pub.publish(base_cmd);
-      printf(" - linear.x= %.2f linear.y= %.2f angular.z= %.2f \n",base_cmd.linear.x,base_cmd.linear.y,base_cmd.angular.z);
-    } 
-    else if(cKey=='d')
-    {
+      printf(" - linear.x= %.2f linear.y= %.2f angular.z= %.2f \n",
+             base_cmd.linear.x, base_cmd.linear.y, base_cmd.angular.z);
+    } else if (cKey == 'd') {
       base_cmd.angular.z += -angular_vel;
-      if(base_cmd.angular.z < min_angular_vel) base_cmd.angular.z = min_angular_vel;
+      if (base_cmd.angular.z < min_angular_vel)
+        base_cmd.angular.z = min_angular_vel;
       cmd_vel_pub.publish(base_cmd);
-      printf(" - linear.x= %.2f linear.y= %.2f angular.z= %.2f \n",base_cmd.linear.x,base_cmd.linear.y,base_cmd.angular.z);
-    } 
-    else if(cKey==' ')
-    {
+      printf(" - linear.x= %.2f linear.y= %.2f angular.z= %.2f \n",
+             base_cmd.linear.x, base_cmd.linear.y, base_cmd.angular.z);
+    } else if (cKey == ' ') {
       base_cmd.linear.x = 0;
       base_cmd.linear.y = 0;
       base_cmd.angular.z = 0;
       cmd_vel_pub.publish(base_cmd);
-      printf(" - linear.x= %.2f linear.y= %.2f angular.z= %.2f \n",base_cmd.linear.x,base_cmd.linear.y,base_cmd.angular.z);
-    } 
-    else if(cKey=='x')
-    {
+      printf(" - linear.x= %.2f linear.y= %.2f angular.z= %.2f \n",
+             base_cmd.linear.x, base_cmd.linear.y, base_cmd.angular.z);
+    } else if (cKey == 'x') {
       base_cmd.linear.x = 0;
       base_cmd.linear.y = 0;
       base_cmd.angular.z = 0;
       cmd_vel_pub.publish(base_cmd);
-      printf(" - linear.x= %.2f linear.y= %.2f angular.z= %.2f \n",base_cmd.linear.x,base_cmd.linear.y,base_cmd.angular.z);
+      printf(" - linear.x= %.2f linear.y= %.2f angular.z= %.2f \n",
+             base_cmd.linear.x, base_cmd.linear.y, base_cmd.angular.z);
       printf("Exit! \n");
       return 0;
-    } 
-    else
-    {
-       printf(" - Undefined Instruction \n");
+    } else {
+      printf(" - Undefined Instruction \n");
     }
-    
   }
   return 0;
 }
