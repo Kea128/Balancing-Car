28cd8d0d8c6365420346567c26607c75769741a6
GYK <1287811578@qq.com>
2024-07-20 15:03:38 +0800

    ver_0720_xacro理解完毕

--- src/balancing_car_control/src/sensor.cpp
+++ src/balancing_car_control/src/sensor.cpp
@@ -1,36 +1,40 @@
 #include "balancing_car_control/control_param.h"
 #include "ros/ros.h"
 #include "sensor_msgs/Imu.h"
-#include "sensor_msgs/JointState.h"
-#include "std_msgs/Float64MultiArray.h"
-#include "tf/tf.h"
+#include "sensor_msgs/JointState.h"  // 包含关节状态消息类型的头文件
+#include "std_msgs/Float64MultiArray.h"  // 包含标准消息类型 Float64MultiArray 的头文件，用于发布轮子扭矩
+#include "tf/tf.h"  // 包含 TF 变换的头文件，用于四元数和欧拉角的转换。
 
-ros::ServiceClient client;        // Client requesting control solution
-ros::Publisher wheelsCommandPub;  // Publish wheel torque
+ros::ServiceClient client;  // 服务客户端，用于请求控制解决方案
+ros::Publisher wheelsCommandPub;  // 发布者，用于发布轮子扭矩命令
 
 double lastPitch,
     lastYaw;  // The previous Euler angle is used to obtain the angular velocity
-double leftWheelVelocity, rightWheelVelocity;
-double dst_vel;
-double dst_yaw;
+double leftWheelVelocity, rightWheelVelocity;  // 左右轮的速度
+double dst_vel;                                // 目标速度
+double dst_yaw;                                // 目标偏航角
 
 // Subscribe the speed of two rounds
+// 接收和处理轮子速度信息
 void WheelVelocityCallback(const sensor_msgs::JointState::ConstPtr& msg) {
-  if (msg->velocity.size() > 0) {
-    leftWheelVelocity = msg->velocity[0];
-    rightWheelVelocity = msg->velocity[1];
+  if (msg->velocity.size() > 0) {  // 检查消息中是否有速度信息
+    leftWheelVelocity = msg->velocity[0];   // 获取左轮速度
+    rightWheelVelocity = msg->velocity[1];  // 获取右轮速度
   }
 }
 
 // Subscribe keyboard control information
+// 回调函数，用于接收和处理键盘控制命令
 void KeyboardCmd(geometry_msgs::Twist msg) {
-  dst_vel = msg.linear.x;
-  dst_yaw = msg.angular.z;
+  dst_vel = msg.linear.x;   //  获取目标速度
+  dst_yaw = msg.angular.z;  // 获取目标偏航角
 }
 
 // Subscribe IMU data
+// 用于接收和处理 IMU 数据。
 void IMUCallback(sensor_msgs::Imu msg) {
-  if (msg.orientation_covariance[0] < 0) return;
+  if (msg.orientation_covariance[0] < 0)
+    return;  //检查 IMU 数据的有效性，若无效则返回
 
   // Convert quaternions in message packets to quaternions in tf
   tf::Quaternion quaternion(msg.orientation.x, msg.orientation.y,
